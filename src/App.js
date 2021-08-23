a2b64c5ce082df91b68e009a324b9e28843ad3da
OhmniD <dave@ohmnid.com>
2021-08-23 17:10:23 +0100

    filters so only users own repos are retrieved

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import { Octokit } from "@octokit/core";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [repos, setRepos] = useState([]);
+	const [repos, setRepos] = useState([]);
 
-  useEffect(() => {
-    getRepos();
-  }, []);
+	useEffect(() => {
+		getRepos();
+	}, []);
 
-  const octokit = new Octokit({
-    auth: pat.key,
-  });
+	const octokit = new Octokit({
+		auth: pat.key,
+	});
 
-  const getRepos = async () => {
-    const repos = await octokit.request("GET /user/repos", { per_page: 100 });
-    setRepos(repos.data);
-    // console.log(repos);
-  };
+	const getRepos = async () => {
+		const repos = await octokit.request("GET /user/repos", { per_page: 100 });
 
-  return (
-    <div className="m-6">
-      <div className="pb-5 border-b border-gray-200">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">
-          Mark GitHub repositories as private
-        </h3>
-      </div>
-      {repos.length > 0 ? (
-        <RepoTable repos={repos} octokit={octokit} setRepos={setRepos} />
-      ) : null}
-    </div>
-  );
+		setRepos(repos.data.filter((repo) => repo.owner.login === pat.username));
+		// console.log(repos);
+	};
+
+	return (
+		<div className="m-6">
+			<div className="pb-5 border-b border-gray-200">
+				<h3 className="text-lg leading-6 font-medium text-gray-900">
+					Mark GitHub repositories as private
+				</h3>
+			</div>
+			{repos.length > 0 ? (
+				<RepoTable repos={repos} octokit={octokit} setRepos={setRepos} />
+			) : null}
+		</div>
+	);
 }
 
 export default App;
